f88581ede47f5431fd5d1523f7a32dd2347bee08
Jasmine Duruji <jasminedrj@gmail.com>
2021-10-12 22:23:15 -0400

    commit tree 2a9acd986cb7a1e24c82133a3fb75371dd752758

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+}
+customerName();
+
+function setBestCustomer (){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bill";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "will";
+}
